3687b3286c901fb3b7b424c4c5ea9d0da08ce2fb
Esther Zinn <Bionicfemme@me.com>
2015-03-23 09:56:50 -0400

    AWS is now configured with paperclip

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,16 @@ class Game < ActiveRecord::Base
       has_attached_file :image
       validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
       has_attached_file :resource
+
+      validates_attachment_content_type :image, 
+      content_type:  /^image\/(png|gif|jpeg)/,
+      message: "Only images allowed"
+
+      validates_attachment_content_type :resource,
+      content_type: ['application/pdf'],
+      message: "Only pdfs allowed"
+
+      validates :image, attachment_presence: true
+      validates :resource, attachment_presence: true
+
 end
